a291148a7638b5ea91ac563cbf66c0ea1d312ad6
IngMyr <ingrid.myrann@gmail.com>
2025-04-25 14:28:58 +0200

    Add base path to vite

--- environment.js
+++ environment.js
@@ -1,12 +1,10 @@
 import * as process from 'node:process';
-// import 'dotenv/config';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
-export const API_BASE_PATH = isDevelopment
-    ? 'http://localhost:8080'
-    : process.env.BASE_PATH || 'beta/fintlabs-no';
-export const PORT = process.env.PORT || '3000';
+export const BASE_PATH = process.env.BASE_PATH || '/beta/fintlabs-no';
+
+export const PORT = process.env.PORT || '8000';
 export const USER_API_URL = process.env.USER_API_URL || 'http://localhost:8062';
 export const CONSENT_API_URL = isDevelopment
     ? 'http://localhost:8080'
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { BASE_PATH } from './environment';
 
 function mockServerPlugin() {
     return {
@@ -20,5 +21,6 @@ function mockServerPlugin() {
 }
 
 export default defineConfig({
+    base: `${BASE_PATH === '/' ? '' : BASE_PATH}/`,
     plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), mockServerPlugin()],
 });
